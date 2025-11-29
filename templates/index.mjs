9bff9c738b689354df9ba148ba205a319e66e649
Erchius <zeyue_jia@u.nus.edu>
2025-11-29 23:41:38 +0800

    update index

--- templates/index.mjs
+++ templates/index.mjs
@@ -3,7 +3,7 @@ const head = pathname.startsWith("/"), tail = pathname.endsWith("/");
 
 const position = pathname.slice(head ? 1 : 0, tail ? -1 : +Infinity);
 Promise.all(
-    [import(new URL(`/ast/${position}.json`, location.origin), { with: { type: "json" } })
+    [import(new URL(`/ast/${position ? position : "index" }.json`, location.origin), { with: { type: "json" } })
         , import("/ast-ef.mjs")
     ]
 ).then(([{ default: [ast, warning] }, { ASTnode2DOM_EF, container, sectionLink }]) => {
